5a687a6b5e9589b0f9315438ce54efc53997a983
Nick Foden <nickfoden@gmail.com>
2017-06-28 23:58:01 -0400

    Added BURNING level of closesness

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ export default class Layout extends React.Component {
 			currentGuess: '',
 			history: [],
 			target: Math.round(Math.random()*100)
-		}
+		};
 	}
 
 	new(e){
@@ -23,49 +23,56 @@ export default class Layout extends React.Component {
 			currentGuess: '',
 			history: [],
 			target: Math.round(Math.random()*100)
-		})
+		});
 	}
 
 	onGuess(guess){
-		console.log(this.state.target);
-		this.setState({
-			currentGuess:guess,
-			history: [...this.state.history, guess]
-		})
+		console.log(typeof guess);
+		if (isNaN(parseInt(guess, 10))) {
+			this.setState({
+				temp: "Please enter a #"
+			})
+			return
+		}
+		console.log("target " + this.state.target);
 
-		let result = Math.abs(this.state.currentGuess - this.state.target);
+		this.setState({
+			currentGuess: guess,
+			history: [...this.state.history, guess]
+		});
+
+		console.log("Guess is " + guess);
+		let result = Math.abs(this.state.target - guess);
+
+		let feedback;
 
 		if (result >= 25) {
-			this.setState({ 
-				temp: 'FREEZING'
-			})
+			feedback = 'FREEZING'
 		}
 		else if (result >= 15) {
-			this.setState({
-				temp: 'COLD'
-			})
+			feedback = 'COLD'
 		}
 		else if (result >= 10) {
-			this.setState({
-				temp: 'WARM'
-			})
+			feedback = 'WARM'
 		}
-		else if (result >= 4) {
-			this.setState({
-				temp: 'HOT'
-			})
+		else if (result >= 5) {
+			feedback = 'HOT'
+		}
+		else if (result >= 1) {
+			feedback = 'BURNING'
 		}
 		else if (result === 0) {
-			this.setState({
-				temp: 'BINGO'
-			})
+			feedback = 'BINGO'
 		}
+		this.setState({
+			temp: feedback
+		})	
 	}	
 
 	render () {
 		return (
 			<div className="Layout">
-				<Header new={()=> this.new()}/>
+				<Header new={()=> this.new()} />
 				<Body 
 					temp={this.state.temp}
 					onGuess={(guess) => this.onGuess(guess)}
